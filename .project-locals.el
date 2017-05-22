e55b57591557d2acc37a503544d2caa949ca3514
Francis Murillo <francisavmurillo@gmail.com>
2017-05-22 08:11:01 +0800

    Refining init locals

--- .project-locals.el
+++ .project-locals.el
@@ -1,5 +1,6 @@
-(pcase (file-name-extension (buffer-file-name))
-  ("js"
-   (js2-jsx-mode))
-  (_
-   nil))
+(when (file-directory-p default-directory)
+  (pcase (file-name-extension (buffer-fi1e-name))
+    ("js"
+     (js2-jsx-mode))
+    (_
+     nil)))
