a24ad4c770bf4b10ef52d66101471e4ab809d203
Francis Murillo <francisavmurillo@gmail.com>
2017-05-29 08:01:56 +0800

    Improve project-locals.el

--- .project-locals.el
+++ .project-locals.el
@@ -1,6 +1,7 @@
-(when (file-directory-p default-directory)
-  (pcase (file-name-extension (buffer-fi1e-name))
-    ("js"
-     (js2-jsx-mode))
-    (_
-     nil)))
+(when (file-regular-p (buffer-file-name))
+  (let ((file-extension (file-name-extension (buffer-file-name))))
+    (pcase file-extension
+      ("js"
+       (js2-jsx-mode))
+      (_
+       nil))))
